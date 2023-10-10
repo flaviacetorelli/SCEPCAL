27de703f800cd977790dd40240ecf45e6076ac7b
wonyongc <wonyongc@gmail.com>
2023-10-09 20:44:40 -0400

    add flake.nix for nix systems

--- flake.nix
+++ flake.nix
@@ -4,40 +4,23 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/22.11";
     utils.url = "github:numtide/flake-utils";
-    hepnix.url = "github:wonyongc/hepnix";
-    hepnix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, utils, hepnix, ... }@inputs:
+  outputs = { self, nixpkgs, utils, ... }@inputs:
 
-    inputs.utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ]
+    inputs.utils.lib.eachSystem [ "x86_64-linux" ]
 
     (system: let
       pkgname = "SCEPCAL";
 
-      overlays = hepnix.overlays;
+      hepcore = import /home/wonyongc/src/hep/hepnix/hepcore.nix;
+
+      overlays = hepcore.overlays;
 
       pkgs = import nixpkgs {
         inherit system overlays;
         config.allowUnfree = true;
       };
-
-      overlaid = ps: with ps; [
-        podio
-        edm4hep
-        VecCore
-        VecGeom
-        vdt
-        SIO
-        LCIO
-        geant4
-        dd4hep
-        evtgen
-        gaudi
-        K4FWCore
-        k4SimGeant4
-        k4Gen
-      ];
 
       in {
 
@@ -47,9 +30,16 @@
 
           src = self;
 
-          buildInputs = hepnix.core pkgs ++ overlaid pkgs;
+          buildInputs = hepcore.core pkgs ++ (with pkgs; [ AIDA podio edm4hep VecCore VecGeom vdt SIO LCIO mygeant4 dd4hep evtgen hepPDT gaudi k4FWCore k4SimGeant4 k4Gen ]);
 
-          nativeBuildInputs = hepnix.wrappers pkgs;
+          nativeBuildInputs = hepcore.wrappers pkgs;
+
+          prePhases = [ "sourcePhase" ];
+
+          sourcePhase = ''
+          source ${pkgs.mygeant4}/bin/geant4.sh
+          source ${pkgs.dd4hep}/bin/thisdd4hep.sh
+          '';
         };
       }
     );
